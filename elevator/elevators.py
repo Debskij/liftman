3d42da5921e4d986daea1694b1a96ae03b7f7703
Debskij <debskij99@gmail.com>
2021-05-27 18:23:04 +0200

    Satisfy linter

--- elevator/elevators.py
+++ elevator/elevators.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from typing import List
-from bisect import insort_right
 
 
 @dataclass
@@ -12,14 +11,12 @@ class ElevatorDc:
 
 class Elevator:
     def __init__(self):
-        self.cur_position = 0
-        self.cur_direction = 0
-        self.stops = []
+        self.cur_position: int = 0
+        self.cur_direction: int = 0
+        self.stops: List[int] = []
 
     def __str__(self):
-        return f'pos: {self.cur_position}\n' \
-               f'dir: {self.cur_direction}\n' \
-               f'stops: {self.stops}'
+        return f"pos: {self.cur_position}\n" f"dir: {self.cur_direction}\n" f"stops: {self.stops}"
 
 
 class ElevatorOperator:
@@ -30,9 +27,9 @@ class ElevatorOperator:
         self.next_stop = [0 for _ in range(number_of_elevators)]
         self.final_stop = [0 for _ in range(number_of_elevators)]
         self.positions = [0 for _ in range(number_of_elevators)]
-        self.stops = [list() for _ in range(number_of_elevators)]
+        self.stops: List[List[int]] = [[] for _ in range(number_of_elevators)]
         self.vector = [0 for _ in range(number_of_elevators)]
-        self.waiting_list = []
+        self.waiting_list: List[List[int]] = []
 
     def step(self):
         travel_dist = {abs(self.next_stop[idx] - self.positions[idx]) for idx in range(self.elevators)}
@@ -46,8 +43,10 @@ class ElevatorOperator:
                 self.positions[idx] += travel_dist * self.vector
                 if self.positions[idx] == self.next_stop[idx]:
                     if len(self.stops[idx]):
-                        distances = [(in_idx, abs(self.positions[idx] - self.stops[idx][in_idx]))
-                                     for in_idx in range(len(self.stops[idx]))]
+                        distances = [
+                            (in_idx, abs(self.positions[idx] - self.stops[idx][in_idx]))
+                            for in_idx in range(len(self.stops[idx]))
+                        ]
                         in_idx, _ = min(distances, key=lambda x: x[1])
                         self.next_stop[idx] = self.stops[idx][in_idx]
                         self.stops[idx] = list(filter(lambda x: x != self.stops[idx][in_idx], self.stops[idx]))
@@ -56,9 +55,13 @@ class ElevatorOperator:
 
     def find_which_elevators(self, pos: int, destination: int) -> List[int]:
         vector = 1 if pos - destination > 0 else -1
-        return [idx for idx in range(self.elevators) if
-                min(self.positions[idx], self.final_stop[idx]) <= pos <= max(self.positions[idx], self.final_stop[idx])
-                and self.vector == vector]
+
+        return [
+            idx
+            for idx in range(self.elevators)
+            if min(self.positions[idx], self.final_stop[idx]) <= pos <= max(self.positions[idx], self.final_stop[idx])
+            and self.vector == vector
+        ]
 
     def find_closest(self, indexes: List[int], pos: int) -> int:
         return min(indexes, key=lambda x: abs(self.positions[x] - pos))
@@ -67,5 +70,5 @@ class ElevatorOperator:
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     e = ElevatorOperator(3)
